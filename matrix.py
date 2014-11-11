813188ae39e961735d5beb75799ed9f6cf9aaa45
Prasanna Khanapur <pskhanapur@gmail.com>
2014-11-11 17:20:25 +0100

    Update matrix.py
    comments added

--- matrix.py
+++ matrix.py
@@ -38,27 +38,25 @@ class Matrix( Topo ):
         leftBottomHost = self.addHost( 'h3' )
         rightBottomHost = self.addHost( 'h4' )
 
-
         switches = []
         for i in range(1,n+1):
             for j in range(1,n+1):
                 switches.append(self.addSwitch( 's' + str(j+((i-1)*n))))
-
+                
         # Add links
-        #self.addLink( leftHost, leftSwitch )
-
         for i in range(1,n+1):
             for j in range(1,n):
                 firstswitch = 's'+ str(j+((i-1)*n))
                 secondswitch = 's'+ str(j+((i-1)*n)+1)
                 self.addLink(firstswitch,secondswitch)
+                
         for i in range(1,n):
             for j in range(1,n+1):
                 firstswitch = 's'+ str(j+((i-1)*n))
                 secondswitch = 's'+ str(j+n+((i-1)*n))
                 self.addLink(firstswitch,secondswitch)
 
-        # Add links
+       
         self.addLink( leftTopHost, 's1' )
         self.addLink( rightTopHost, 's'+ str(n))
         self.addLink( leftBottomHost, 's'+ str(n*n-n+1))
